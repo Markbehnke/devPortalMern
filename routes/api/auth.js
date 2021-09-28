7f12418f35f074b4b92c282a21b89cc5207f6bfe
Mark B <mark.behnke1998@gmail.com>
2021-09-28 11:58:28 -0700

    Started endpoints

---
+++ routes/api/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+
+// @route       GET api/auth
+//@description  test route
+//@access       public
+router.get('/', (req, res) => res.send('Auth router'));
+
+module.exports = router;
